8ee05fa2d51c35ed5b897a5c2fbe025bdb99c946
Garett Tok Ern Liang <garettok@hotmail.com>
2019-09-09 10:24:34 -0400

    Added pagination support for facebook

--- src/components/views/Facebook/FacebookHome.jsx
+++ src/components/views/Facebook/FacebookHome.jsx
@@ -13,30 +13,53 @@ import { getAllUsers } from "../../../api/users";
 import { checkAndHandleError } from "../../../lib/general";
 import { Link } from "react-router5";
 import { userTypeStudent } from "../../../constants/general";
+import PaginationButtons from "../../PaginationButtons";
 
 const FacebookHome = ({ token, route }) => {
   const [results, updateResults] = useState(null);
+  const perPage = 20;
+  const [page, updatePage] = useState(0);
+  const [total, updateTotal] = useState(0);
+
+  // loads the next set of users
+  const loadUsers = async (newPage) => {
+    if (!route.params.q) {
+      updateResults([]);
+      return;
+    }
+
+    const queryParams = {
+      q: route.params.q,
+      limit: perPage,
+      offset: perPage * newPage,
+      preload: ["dorm", "office"],
+    };
+    const resultsResponse = await getAllUsers(token, queryParams);
+
+    if (checkAndHandleError(resultsResponse)) {
+      updateResults(resultsResponse.data.data);
+      // @TODO: update when pagination support is done
+      updateTotal(resultsResponse.data.paginationTotal);
+    } else {
+      updateResults([]);
+      updateTotal(0);
+    }
+  };
 
   useEffect(() => {
-    const loadUsers = async () => {
-      if (!route.params.q) {
-        updateResults([]);
-        return;
-      }
-
-      const queryParams = {
-        q: route.params.q,
-        preload: ["dorm", "office"],
-      };
-      const resultsResponse = await getAllUsers(token, queryParams);
-
-      if (checkAndHandleError(resultsResponse)) {
-        updateResults(resultsResponse.data.data);
-      } else updateResults([]);
-    };
-
-    loadUsers();
+    loadUsers(0);
   }, [token, route.params.q]);
+
+  // Handles clicking of the next/previous page
+  const clickHandler = (number) => {
+    if (number === -1 && page > 0) {
+      loadUsers(page - 1);
+      updatePage(page - 1);
+    } else if (number === 1 && total - (page + 1) * perPage > 0) {
+      loadUsers(page + 1);
+      updatePage(page + 1);
+    }
+  };
 
   // Generates the user's room
   const listUserRoom = (user) => {
@@ -52,33 +75,41 @@ const FacebookHome = ({ token, route }) => {
   // Displays results in a list view when there are too many results
   const ListView = () => {
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th className="unix-column">Unix</th>
-            <th>Room/Office</th>
-          </tr>
-        </thead>
-        <tbody>
-          {results.map((user) => {
-            return (
-              <tr key={user.id}>
-                <td>
-                  <Link
-                    routeName="facebook.users"
-                    routeParams={{ userID: user.id }}
-                  >
-                    {user.name}
-                  </Link>
-                </td>
-                <td>{user.unixID}</td>
-                <td>{listUserRoom(user)}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th className="unix-column">Unix</th>
+              <th>Room/Office</th>
+            </tr>
+          </thead>
+          <tbody>
+            {results.map((user) => {
+              return (
+                <tr key={user.id}>
+                  <td>
+                    <Link
+                      routeName="facebook.users"
+                      routeParams={{ userID: user.id }}
+                    >
+                      {user.name}
+                    </Link>
+                  </td>
+                  <td>{user.unixID}</td>
+                  <td>{listUserRoom(user)}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <PaginationButtons
+          clickHandler={clickHandler}
+          page={page}
+          total={total}
+          perPage={perPage}
+        />
+      </>
     );
   };
 
