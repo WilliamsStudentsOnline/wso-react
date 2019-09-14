2d7f79f7c907250bbb039dee1c52659bf3328197
Garett Tok Ern Liang <garettok@hotmail.com>
2019-09-14 11:56:30 -0400

    Updated factrak AOS

--- src/components/views/Factrak/FactrakAOS.jsx
+++ src/components/views/Factrak/FactrakAOS.jsx
@@ -25,6 +25,7 @@ const FactrakAOS = ({ route, token }) => {
   useEffect(() => {
     const areaParam = route.params.area;
 
+    // Loads professors of the Area of Study
     const loadProfs = async (areaOfStudyID) => {
       const params = { areaOfStudyID };
       const profsResponse = await getProfessors(token, params);
@@ -33,6 +34,7 @@ const FactrakAOS = ({ route, token }) => {
       }
     };
 
+    // Loads courses of the Area of Study
     const loadCourses = async (areaOfStudyID) => {
       const params = { areaOfStudyID, preload: ["professors"] };
       const coursesResponse = await getCourses(token, params);
@@ -42,6 +44,7 @@ const FactrakAOS = ({ route, token }) => {
       }
     };
 
+    // Loads additional information regarding the area of study
     const loadAOS = async (areaID) => {
       const areaOfStudyResponse = await getAreaOfStudy(token, areaID);
       if (checkAndHandleError(areaOfStudyResponse)) {
@@ -54,49 +57,96 @@ const FactrakAOS = ({ route, token }) => {
     loadAOS(areaParam);
   }, [route.params.area, token]);
 
-  return (
-    <article className="factrak-home">
-      <section className="margin-vertical-small">
-        <h3>{area && area.name ? area.name : ""}</h3>
-        {profs.length > 0 ? (
-          <>
-            <br />
-            <h4>{`Professors in ${area && area.name ? area.name : ""}`}</h4>
-            <table>
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Title</th>
-                  <th className="unix-column">Unix</th>
-                </tr>
-              </thead>
-              <tbody>
-                {profs.map((prof) => (
-                  <tr key={prof.id}>
-                    <td>
-                      <Link
-                        routeName="factrak.professors"
-                        routeParams={{ profID: prof.id }}
-                      >
-                        {prof.name}
-                      </Link>
-                    </td>
+  // Generates a row containing the prof information.
+  const generateProfRow = (prof) => {
+    return (
+      <tr key={prof.id}>
+        <td>
+          <Link
+            routeName="factrak.professors"
+            routeParams={{ profID: prof.id }}
+          >
+            {prof.name}
+          </Link>
+        </td>
 
-                    <td>{prof.title}</td>
-                    <td>
-                      <a href={`mailto:${prof.unixID}@williams.edu`}>
-                        {prof.unixID}
-                      </a>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </>
-        ) : null}
-      </section>
+        <td>{prof.title}</td>
+        <td>
+          <a href={`mailto:${prof.unixID}@williams.edu`}>{prof.unixID}</a>
+        </td>
+      </tr>
+    );
+  };
 
-      <section className="margin-vertical-small">
+  // Generates the component which holds the list of professors in the area of study
+  const generateProfs = () => {
+    // If no profs were found, return null. Should not happen for Area of Study unless it's new.
+    if (profs.length === 0) return null;
+    return (
+      <>
+        <br />
+        <h4>{`Professors in ${area && area.name ? area.name : ""}`}</h4>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Title</th>
+              <th className="unix-column">Unix</th>
+            </tr>
+          </thead>
+          <tbody>{profs.map((prof) => generateProfRow(prof))}</tbody>
+        </table>
+      </>
+    );
+  };
+
+  // Generate a course's professors' information
+  const generateCourseProfessors = (course) => {
+    if (course.professors) {
+      return course.professors
+        .map((prof) => {
+          return (
+            <Link
+              routeName="factrak.courses.singleProf"
+              routeParams={{
+                courseID: course.id,
+                profID: prof.id,
+              }}
+              key={prof.id}
+            >
+              {prof.name}
+            </Link>
+          );
+        })
+        .reduce((prev, curr) => [prev, ", ", curr]);
+    }
+
+    return null;
+  };
+
+  // Generates a row containing the course information.
+  const generateCourseRow = (course) => {
+    return (
+      <tr key={course.id}>
+        <td className="col-20">
+          <Link
+            routeName="factrak.courses"
+            routeParams={{ courseID: course.id }}
+          >
+            {`${area.abbreviation} ${course.number}`}
+          </Link>
+        </td>
+        <td className="col-80">{generateCourseProfessors(course)}</td>
+      </tr>
+    );
+  };
+
+  // Generates the component which holds the list of courses in the area of study
+  const generateCourses = () => {
+    // If no profs were found, return null. Should not happen for Area of Study unless it's new.
+    if (profs.length === 0) return null;
+    return (
+      <>
         <h4>Courses</h4>
         <table>
           <thead>
@@ -105,41 +155,20 @@ const FactrakAOS = ({ route, token }) => {
               <th className="col-80">Professors</th>
             </tr>
           </thead>
-          <tbody>
-            {courses.map((course) => (
-              <tr key={course.id}>
-                <td className="col-20">
-                  <Link
-                    routeName="factrak.courses"
-                    routeParams={{ courseID: course.id }}
-                  >
-                    {`${area.abbreviation} ${course.number}`}
-                  </Link>
-                </td>
-                <td className="col-80">
-                  {course.professors &&
-                    course.professors
-                      .map((prof) => {
-                        return (
-                          <Link
-                            routeName="factrak.courses.singleProf"
-                            routeParams={{
-                              courseID: course.id,
-                              profID: prof.id,
-                            }}
-                            key={prof.id}
-                          >
-                            {prof.name}
-                          </Link>
-                        );
-                      })
-                      .reduce((prev, curr) => [prev, ", ", curr])}
-                </td>
-              </tr>
-            ))}
-          </tbody>
+          <tbody>{courses.map((course) => generateCourseRow(course))}</tbody>
         </table>
+      </>
+    );
+  };
+
+  return (
+    <article className="factrak-home">
+      <section className="margin-vertical-small">
+        <h3>{area && area.name ? area.name : ""}</h3>
+        {generateProfs()}
       </section>
+
+      <section className="margin-vertical-small">{generateCourses()}</section>
     </article>
   );
 };
