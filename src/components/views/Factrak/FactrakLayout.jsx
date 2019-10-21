4c6c2dd91184e77bd977f0c1a18033977bc20568
Garett Tok Ern Liang <garettok@hotmail.com>
2019-10-21 16:28:43 -0400

    factrak autocomplete now closes suggestion box

--- src/components/views/Factrak/FactrakLayout.jsx
+++ src/components/views/Factrak/FactrakLayout.jsx
@@ -23,7 +23,8 @@ const FactrakLayout = ({ children, currUser, navigateTo, token, route }) => {
       else setQuery("");
     };
     loadQuery();
-  }, [route.params.q]);
+    setShowSuggestions(false);
+  }, [route.params.q, route.path]);
 
   // Initiates new autocomplete
   const factrakAutocomplete = async (event) => {
@@ -41,6 +42,7 @@ const FactrakLayout = ({ children, currUser, navigateTo, token, route }) => {
     } else {
       setSuggestions(suggestData);
     }
+    setShowSuggestions(true);
   };
 
   const submitHandler = (event) => {
@@ -76,6 +78,7 @@ const FactrakLayout = ({ children, currUser, navigateTo, token, route }) => {
           routeName="factrak.courses"
           routeParams={{ courseID: suggestion.id }}
           onMouseDown={(e) => e.preventDefault()}
+          routeOptions={{ reload: true }}
         >
           {suggestion.value}
         </Link>
@@ -87,6 +90,7 @@ const FactrakLayout = ({ children, currUser, navigateTo, token, route }) => {
           routeName="factrak.professors"
           routeParams={{ profID: suggestion.id }}
           onMouseDown={(e) => e.preventDefault()}
+          routeOptions={{ reload: true }}
         >
           {suggestion.value}
         </Link>
@@ -102,7 +106,7 @@ const FactrakLayout = ({ children, currUser, navigateTo, token, route }) => {
       <div className="autocomplete">
         <table id="suggestions">
           <tbody>
-            {suggestions !== [] && showSuggestions
+            {suggestions.length > 0 && showSuggestions
               ? suggestions.map((suggestion) => (
                   <tr key={suggestion.value}>
                     <td>{suggestionRow(suggestion)}</td>
