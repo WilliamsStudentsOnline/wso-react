f9779052dd6e58cec3894fa60d749593299aa79d
Garett Tok Ern Liang <36098015+walnutdust@users.noreply.github.com>
2020-02-14 14:32:02 -0500

    Dormtrak Ranking skeleton

--- src/components/views/Dormtrak/DormtrakRanking.jsx
+++ src/components/views/Dormtrak/DormtrakRanking.jsx
@@ -1,6 +1,7 @@
 // React Imports
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import { Line } from "../../Skeleton";
 
 // Redux imports
 import { connect } from "react-redux";
@@ -28,249 +29,262 @@ const DormtrakRanking = ({ token }) => {
 
   const times = [0, 0, 0];
 
+  const rankingSkeleton = () => (
+    <tr>
+      <td>
+        <Line width="45%" />
+        <br />
+        <Line width="55%" />
+      </td>
+      <td>
+        <Line width="45%" />
+        <br />
+        <Line width="55%" />
+      </td>
+    </tr>
+  );
+
   return (
-    <>
-      <article className="dorm-ranks">
-        <h3>Rankings</h3>
+    <article className="dorm-ranks">
+      <h3>Rankings</h3>
 
-        <table>
-          <tbody>
-            <tr>
-              <th>Max Mean Single Size</th>
-              <th>Min Mean Single Size</th>
-            </tr>
-            {dormInfo
-              ? times.map((element, index) => {
-                  const a = dormInfo.maxMeanSingleSize[index];
-                  const b = dormInfo.minMeanSingleSize[index];
-                  return (
-                    <tr key={b.averageSingleArea + a.averageSingleArea}>
-                      <td>
-                        <Link
-                          routeName="dormtrak.dorms"
-                          routeParams={{ dormID: a.id }}
-                        >
-                          {a.name}
-                        </Link>
-                        <br />
-                        {`(${a.averageSingleArea} sq. ft.)`}
-                      </td>
-                      <td>
-                        <Link
-                          routeName="dormtrak.dorms"
-                          routeParams={{ dormID: b.id }}
-                        >
-                          {b.name}
-                        </Link>
-                        <br />
-                        {`(${b.averageSingleArea} sq. ft.)`}
-                      </td>
-                    </tr>
-                  );
-                })
-              : null}
-          </tbody>
-        </table>
+      <table>
+        <tbody>
+          <tr>
+            <th>Max Mean Single Size</th>
+            <th>Min Mean Single Size</th>
+          </tr>
+          {times.map((_, index) => {
+            if (!dormInfo) return rankingSkeleton();
 
-        <table>
-          <tbody>
-            <tr>
-              <th>Biggest Singles</th>
-              <th>Smallest Singles</th>
-            </tr>
-            {dormInfo
-              ? times.map((element, index) => {
-                  const a = dormInfo.biggestSingles[index];
-                  const b = dormInfo.smallestSingles[index];
-                  return (
-                    <tr key={a.area}>
-                      <td>
-                        <Link
-                          routeName="dormtrak.dorms"
-                          routeParams={{ dormID: a.dormID }}
-                        >
-                          {`${a.dorm.name} ${a.number}`}
-                        </Link>
-                        <br />
-                        {`(${a.area} sq. ft.)`}
-                      </td>
-                      <td>
-                        <Link
-                          routeName="dormtrak.dorms"
-                          routeParams={{ dormID: b.dormID }}
-                        >
-                          {`${b.dorm.name} ${b.number}`}
-                        </Link>
-                        <br />
-                        {`(${b.area} sq. ft.)`}
-                      </td>
-                    </tr>
-                  );
-                })
-              : null}
-          </tbody>
-        </table>
+            const a = dormInfo.maxMeanSingleSize[index];
+            const b = dormInfo.minMeanSingleSize[index];
+            return (
+              <tr key={b.averageSingleArea + a.averageSingleArea}>
+                <td>
+                  <Link
+                    routeName="dormtrak.dorms"
+                    routeParams={{ dormID: a.id }}
+                  >
+                    {a.name}
+                  </Link>
+                  <br />
+                  {`(${a.averageSingleArea} sq. ft.)`}
+                </td>
+                <td>
+                  <Link
+                    routeName="dormtrak.dorms"
+                    routeParams={{ dormID: b.id }}
+                  >
+                    {b.name}
+                  </Link>
+                  <br />
+                  {`(${b.averageSingleArea} sq. ft.)`}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
 
-        <table>
-          <tbody>
-            <tr>
-              <th>Max Mean Double Size</th>
-              <th>Min Mean Double Size</th>
-            </tr>
-            {dormInfo
-              ? times.map((element, index) => {
-                  const a = dormInfo.maxMeanDoubleSize[index];
-                  const b = dormInfo.minMeanDoubleSize[index];
-                  return (
-                    <tr key={a.averageDoubleArea}>
-                      <td>
-                        <Link
-                          routeName="dormtrak.dorms"
-                          routeParams={{ dormID: a.id }}
-                        >
-                          {a.name}
-                        </Link>
-                        <br />
-                        {`(${a.averageDoubleArea} sq. ft.)`}
-                      </td>
-                      <td>
-                        <Link
-                          routeName="dormtrak.dorms"
-                          routeParams={{ dormID: b.id }}
-                        >
-                          {b.name}
-                        </Link>
-                        <br />
-                        {`(${b.averageDoubleArea} sq. ft.)`}
-                      </td>
-                    </tr>
-                  );
-                })
-              : null}
-          </tbody>
-        </table>
+      <table>
+        <tbody>
+          <tr>
+            <th>Biggest Singles</th>
+            <th>Smallest Singles</th>
+          </tr>
+          {times.map((_, index) => {
+            if (!dormInfo) return rankingSkeleton();
 
-        <table>
-          <tbody>
-            <tr>
-              <th>Biggest Doubles</th>
-              <th>Smallest Doubles</th>
-            </tr>
+            const a = dormInfo.biggestSingles[index];
+            const b = dormInfo.smallestSingles[index];
+            return (
+              <tr key={a.area}>
+                <td>
+                  <Link
+                    routeName="dormtrak.dorms"
+                    routeParams={{ dormID: a.dormID }}
+                  >
+                    {`${a.dorm.name} ${a.number}`}
+                  </Link>
+                  <br />
+                  {`(${a.area} sq. ft.)`}
+                </td>
+                <td>
+                  <Link
+                    routeName="dormtrak.dorms"
+                    routeParams={{ dormID: b.dormID }}
+                  >
+                    {`${b.dorm.name} ${b.number}`}
+                  </Link>
+                  <br />
+                  {`(${b.area} sq. ft.)`}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
 
-            {dormInfo
-              ? times.map((element, index) => {
-                  const a = dormInfo.biggestDoubles[index];
-                  const b = dormInfo.smallestDoubles[index];
-                  return (
-                    <tr key={a.area}>
-                      <td>
-                        <Link
-                          routeName="dormtrak.dorms"
-                          routeParams={{ dormID: a.dormID }}
-                        >
-                          {`${a.dorm.name} ${a.number}`}
-                        </Link>
-                        <br />
-                        {`(${a.area} sq. ft.)`}
-                      </td>
-                      <td>
-                        <Link
-                          routeName="dormtrak.dorms"
-                          routeParams={{ dormID: b.dormID }}
-                        >
-                          {`${b.dorm.name} ${b.number}`}
-                        </Link>
-                        <br />
-                        {`(${b.area} sq. ft.)`}
-                      </td>
-                    </tr>
-                  );
-                })
-              : null}
-          </tbody>
-        </table>
+      <table>
+        <tbody>
+          <tr>
+            <th>Max Mean Double Size</th>
+            <th>Min Mean Double Size</th>
+          </tr>
+          {times.map((_, index) => {
+            if (!dormInfo) return rankingSkeleton();
 
-        <table>
-          <tbody>
-            <tr>
-              <th>Most Singles</th>
-              <th>Most Doubles</th>
-            </tr>
+            const a = dormInfo.maxMeanDoubleSize[index];
+            const b = dormInfo.minMeanDoubleSize[index];
+            return (
+              <tr key={a.averageDoubleArea}>
+                <td>
+                  <Link
+                    routeName="dormtrak.dorms"
+                    routeParams={{ dormID: a.id }}
+                  >
+                    {a.name}
+                  </Link>
+                  <br />
+                  {`(${a.averageDoubleArea} sq. ft.)`}
+                </td>
+                <td>
+                  <Link
+                    routeName="dormtrak.dorms"
+                    routeParams={{ dormID: b.id }}
+                  >
+                    {b.name}
+                  </Link>
+                  <br />
+                  {`(${b.averageDoubleArea} sq. ft.)`}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
 
-            {dormInfo
-              ? times.map((element, index) => {
-                  const a = dormInfo.mostSingles[index];
-                  const b = dormInfo.mostDoubles[index];
-                  return (
-                    <tr key={a.numberSingles}>
-                      <td>
-                        <Link
-                          routeName="dormtrak.dorms"
-                          routeParams={{ dormID: a.id }}
-                        >
-                          {a.name}
-                        </Link>
-                        <br />
-                        {`(${a.numberSingles})`}
-                      </td>
-                      <td>
-                        <Link
-                          routeName="dormtrak.dorms"
-                          routeParams={{ dormID: b.id }}
-                        >
-                          {b.name}
-                        </Link>
-                        <br />
-                        {`(${b.numberDoubles})`}
-                      </td>
-                    </tr>
-                  );
-                })
-              : null}
-          </tbody>
-        </table>
+      <table>
+        <tbody>
+          <tr>
+            <th>Biggest Doubles</th>
+            <th>Smallest Doubles</th>
+          </tr>
 
-        <table>
-          <tbody>
-            <tr>
-              <th>Most bathrooms</th>
-              <th>Fewest bathrooms</th>
-            </tr>
+          {times.map((_, index) => {
+            if (!dormInfo) return rankingSkeleton();
 
-            {dormInfo
-              ? times.map((element, index) => {
-                  const a = dormInfo.mostBathrooms[index];
-                  const b = dormInfo.fewestBathrooms[index];
-                  return (
-                    <tr key={a.bathroomRatio}>
-                      <td>
-                        <Link
-                          routeName="dormtrak.dorms"
-                          routeParams={{ dormID: a.id }}
-                        >
-                          {a.name}
-                        </Link>
-                        <br />
-                        {`(${a.bathroomRatio.toPrecision(3)}:1)`}
-                      </td>
-                      <td>
-                        <Link
-                          routeName="dormtrak.dorms"
-                          routeParams={{ dormID: b.id }}
-                        >
-                          {b.name}
-                        </Link>
-                        <br />
-                        {`(${b.bathroomRatio.toPrecision(3)}:1)`}
-                      </td>
-                    </tr>
-                  );
-                })
-              : null}
-          </tbody>
-        </table>
-      </article>
-    </>
+            const a = dormInfo.biggestDoubles[index];
+            const b = dormInfo.smallestDoubles[index];
+            return (
+              <tr key={a.area}>
+                <td>
+                  <Link
+                    routeName="dormtrak.dorms"
+                    routeParams={{ dormID: a.dormID }}
+                  >
+                    {`${a.dorm.name} ${a.number}`}
+                  </Link>
+                  <br />
+                  {`(${a.area} sq. ft.)`}
+                </td>
+                <td>
+                  <Link
+                    routeName="dormtrak.dorms"
+                    routeParams={{ dormID: b.dormID }}
+                  >
+                    {`${b.dorm.name} ${b.number}`}
+                  </Link>
+                  <br />
+                  {`(${b.area} sq. ft.)`}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      <table>
+        <tbody>
+          <tr>
+            <th>Most Singles</th>
+            <th>Most Doubles</th>
+          </tr>
+
+          {times.map((_, index) => {
+            if (!dormInfo) return rankingSkeleton();
+
+            const a = dormInfo.mostSingles[index];
+            const b = dormInfo.mostDoubles[index];
+            return (
+              <tr key={a.numberSingles}>
+                <td>
+                  <Link
+                    routeName="dormtrak.dorms"
+                    routeParams={{ dormID: a.id }}
+                  >
+                    {a.name}
+                  </Link>
+                  <br />
+                  {`(${a.numberSingles})`}
+                </td>
+                <td>
+                  <Link
+                    routeName="dormtrak.dorms"
+                    routeParams={{ dormID: b.id }}
+                  >
+                    {b.name}
+                  </Link>
+                  <br />
+                  {`(${b.numberDoubles})`}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      <table>
+        <tbody>
+          <tr>
+            <th>Most bathrooms</th>
+            <th>Fewest bathrooms</th>
+          </tr>
+
+          {times.map((_, index) => {
+            if (!dormInfo) return rankingSkeleton();
+
+            const a = dormInfo.mostBathrooms[index];
+            const b = dormInfo.fewestBathrooms[index];
+            return (
+              <tr key={a.bathroomRatio}>
+                <td>
+                  <Link
+                    routeName="dormtrak.dorms"
+                    routeParams={{ dormID: a.id }}
+                  >
+                    {a.name}
+                  </Link>
+                  <br />
+                  {`(${a.bathroomRatio.toPrecision(3)}:1)`}
+                </td>
+                <td>
+                  <Link
+                    routeName="dormtrak.dorms"
+                    routeParams={{ dormID: b.id }}
+                  >
+                    {b.name}
+                  </Link>
+                  <br />
+                  {`(${b.bathroomRatio.toPrecision(3)}:1)`}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </article>
   );
 };
 
