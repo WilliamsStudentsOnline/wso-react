d5c9b2e974a8d899ebd19292b56b5aa8e0dc1028
Trang Q. Ngo <tqn1@williams.edu>
2020-03-30 19:25:06 +0700

    added image

--- src/components/views/Errors/FourOhFour.jsx
+++ src/components/views/Errors/FourOhFour.jsx
@@ -1,11 +1,16 @@
 // React imports
 import React from "react";
+import treePic from "../../../assets/images/404error.png";
 
 const FourOhFour = () => {
   return (
     <header>
-      <h1>Whoops! Page not found!</h1>
-      404. Run. Hide. Cease and Desist.
+      <img src={treePic} alt="A tree in Science Quad" />
+      <h1 style={{ color: "green" }}>Whoops! Page not found!</h1>
+      <p style={{ fontFamily: "Arial" }}>
+        {" "}
+        404. Entertain yourself by finding the man in the tree!{" "}
+      </p>
     </header>
   );
 };
