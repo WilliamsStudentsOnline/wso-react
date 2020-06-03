1bc46913602311575bd71adfa94f1fbded324a1c
Garett Tok Ern Liang <36098015+walnutdust@users.noreply.github.com>
2020-06-03 23:18:22 +0800

    Updated banner (#93)

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -31,7 +31,10 @@ const Homepage = ({ navigateTo }) => {
     <div className="home">
       <div className="full-width">
         <div id="join-header">
-          <a href="https://forms.gle/RYeBrHvi776F24sE9">Join us today!</a>
+          <a href="https://docs.google.com/spreadsheets/u/1/d/1ZIvDZpHqvNZkf8dGFXVjfk-Wq0Y9FTG410NJbH_8K8M/htmlview">
+            Consider donating to a fund that supports anti-racism activism
+            today!
+          </a>
         </div>
         <header>
           <div className="logo">
