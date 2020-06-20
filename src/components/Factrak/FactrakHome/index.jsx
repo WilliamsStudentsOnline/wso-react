b3b0532ff45cd1b16ab91c70948acda9665ee8d7
Elijah Tamarchenko <elijahtamarchenko@Elijahs-MacBook-Air.local>
2020-06-20 15:18:41 -0400

    Basic homepage formatting

--- src/components/Factrak/FactrakHome/index.jsx
+++ src/components/Factrak/FactrakHome/index.jsx
@@ -13,6 +13,10 @@ import { actions } from "redux-router5";
 // Additional imports
 import { containsOneOfScopes, scopes } from "../../../lib/general";
 import { Link } from "react-router5";
+import styles from "./FactrakHome.module.scss";
+
+// Elastic imports
+import { EuiFlexGroup, EuiFlexItem } from "@elastic/eui";
 
 const FactrakHome = ({ currUser, navigateTo, token, wso }) => {
   const [areas, updateAreas] = useState(null);
@@ -57,11 +61,24 @@ const FactrakHome = ({ currUser, navigateTo, token, wso }) => {
   }, [navigateTo, token, wso]);
 
   return (
-    <article className="dormtrak">
-      <div className="container">
-        <aside className="sidebar">
-          <article className="home">
-            <h3>Departments</h3>
+    <article className={styles.factrak}>
+      <EuiFlexGroup
+        className="container"
+        direction="rowReverse"
+        justifyContent="space-around"
+      >
+        <EuiFlexItem className="sidebar" grow={2}>
+          <h3>Departments</h3>
+          <div className={styles.departmentCard} description="">
+            <EuiFlexGroup
+              className="squares"
+              direction="row"
+              justifyContent="space-between"
+            >
+              <EuiFlexItem className={styles.square} grow={false} />
+              <EuiFlexItem />
+              <EuiFlexItem className={styles.square} grow={false} />
+            </EuiFlexGroup>
             <ul id="dept_list">
               {areas ? (
                 areas.map((area) => (
@@ -78,12 +95,21 @@ const FactrakHome = ({ currUser, navigateTo, token, wso }) => {
                 <List height="80%" center numRows={46} />
               )}
             </ul>
-          </article>
-        </aside>
+            <EuiFlexGroup
+              className="squares"
+              direction="row"
+              justifyContent="space-between"
+            >
+              <EuiFlexItem className={styles.square} grow={false} />
+              <EuiFlexItem />
+              <EuiFlexItem className={styles.square} grow={false} />
+            </EuiFlexGroup>
+          </div>
+        </EuiFlexItem>
 
-        <article className="main">
+        <EuiFlexItem className="main" grow={3}>
           <section className="lead">
-            <h3>Recent Comments</h3>
+            <h3>Recent Reviews</h3>
             <br />
             <FactrakDeficitMessage currUser={currUser} />
 
@@ -108,8 +134,8 @@ const FactrakHome = ({ currUser, navigateTo, token, wso }) => {
                   </div>
                 ))}
           </section>
-        </article>
-      </div>
+        </EuiFlexItem>
+      </EuiFlexGroup>
     </article>
   );
 };
