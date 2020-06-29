37f3b325053b1aa5f4bbb8120ea279e880b40cb6
Elijah Tamarchenko <elijahtamarchenko@Elijahs-MacBook-Air.local>
2020-06-29 15:19:10 -0400

    Fixed Factrak Policy Page

--- src/components/Factrak/FactrakPolicy/index.jsx
+++ src/components/Factrak/FactrakPolicy/index.jsx
@@ -9,7 +9,7 @@ import { connect } from "react-redux";
 import { getCurrUser, getWSO } from "../../../selectors/auth";
 
 // Elastic imports
-import { EuiFlexGroup, EuiFlexItem } from "@elastic/eui";
+import { EuiText } from "@elastic/eui";
 
 const FactrakPolicy = ({ currUser, navigateTo, wso }) => {
   const [acceptPolicy, updateAcceptPolicy] = useState(false);
@@ -51,91 +51,68 @@ const FactrakPolicy = ({ currUser, navigateTo, wso }) => {
   return (
     <div>
       <section>
-        <EuiFlexGroup
-          direction="column"
-          className={styles.factrakPolicy}
-          alignItems="center"
-        >
-          <EuiFlexItem>
-            <h1>Policy</h1>
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <br />
-            <h4>IMPORTANT &mdash; new in 2016:</h4>
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <p>
-              <strong>
-                Factrak is only helpful if we have a lot of recent reviews from
-                lots of different people, so we&apos;ve introduced a few
-                changes.
-              </strong>
-            </p>
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <p>
-              In order to access Factrak,{" "}
-              <strong>
-                you must contribute at least 2 reviews per semester.{" "}
-              </strong>
-              This requirement is waived if you have reviewed at least all but 2
-              of your courses in your time at Williams. The requirement is not
-              retroactive. The year is divided into 2 periods: March -
-              September, and October - February. To view Factrak during one of
-              these two periods, the above requirements must have been met
-              during that period. For example, to see Factrak in December, you
-              must have submitted at least 2 reviews since October.
-            </p>
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <p>
-              Factrak now supports bubble-sheet style reviews for courses and
-              professors. This is entirely optional, and you can still submit
-              just the traditional text comment. The statistics gathered will
-              not be visible until enough data is collected.
-            </p>
-            <br />
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <p>
-              To create a forum where students can openly and honestly share
-              their opinions of their professors in a manner that is anonymous
-              and only visible to other students. This feedback is primarily
-              designed to help other students find courses and professors which
-              are the best matches for them.
-            </p>
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <p>
-              While Factrak is designed to be as open as possible, inappropriate
-              comments will not be tolerated, and comments can be removed or
-              edited at the discretion of the student moderator to bring them
-              into accordance with this policy. Inappropriate comments include,
-              but are not limited to, comments that name other students, that
-              contain racial or ethnic slurs, and those that harass others.
-              Users may flag comments for moderator review using the button at
-              the bottom of each comment. Use this if you feel the comment has
-              violated the acceptable use policy.
-            </p>
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <p>
-              By using Factrak users agree to abide by this policy. The use of
-              Factrak is also governed by the wider WSO policy and the{" "}
-              <a href="http://oit.williams.edu/w/?u=docs/Computing+Ethics+and+Responsibilities">
-                OIT Computing Ethics and Responsibilities Statement
-              </a>
-              .
-            </p>
-          </EuiFlexItem>
+        <EuiText textAlign="center" className={styles.policyText}>
+          <h1>Policy</h1>
+          <br />
+          <h4>IMPORTANT &mdash; new in 2016:</h4>
+          <p>
+            <strong>
+              Factrak is only helpful if we have a lot of recent reviews from
+              lots of different people, so we&apos;ve introduced a few changes.
+            </strong>
+          </p>
+          <p>
+            In order to access Factrak,{" "}
+            <strong>
+              you must contribute at least 2 reviews per semester.{" "}
+            </strong>
+            This requirement is waived if you have reviewed at least all but 2
+            of your courses in your time at Williams. The requirement is not
+            retroactive. The year is divided into 2 periods: March - September,
+            and October - February. To view Factrak during one of these two
+            periods, the above requirements must have been met during that
+            period. For example, to see Factrak in December, you must have
+            submitted at least 2 reviews since October.
+          </p>
+          <p>
+            Factrak now supports bubble-sheet style reviews for courses and
+            professors. This is entirely optional, and you can still submit just
+            the traditional text comment. The statistics gathered will not be
+            visible until enough data is collected.
+          </p>
+          <br />
+          <p>
+            To create a forum where students can openly and honestly share their
+            opinions of their professors in a manner that is anonymous and only
+            visible to other students. This feedback is primarily designed to
+            help other students find courses and professors which are the best
+            matches for them.
+          </p>
+          <p>
+            While Factrak is designed to be as open as possible, inappropriate
+            comments will not be tolerated, and comments can be removed or
+            edited at the discretion of the student moderator to bring them into
+            accordance with this policy. Inappropriate comments include, but are
+            not limited to, comments that name other students, that contain
+            racial or ethnic slurs, and those that harass others. Users may flag
+            comments for moderator review using the button at the bottom of each
+            comment. Use this if you feel the comment has violated the
+            acceptable use policy.
+          </p>
+          <p>
+            By using Factrak users agree to abide by this policy. The use of
+            Factrak is also governed by the wider WSO policy and the{" "}
+            <a href="http://oit.williams.edu/w/?u=docs/Computing+Ethics+and+Responsibilities">
+              OIT Computing Ethics and Responsibilities Statement
+            </a>
+            .
+          </p>
           {currUser.hasAcceptedFactrakPolicy ? (
-            <EuiFlexItem>
-              <p>
-                <b>You have already accepted the Factrak policy.</b>
-              </p>
-            </EuiFlexItem>
+            <p>
+              <b>You have already accepted the Factrak policy.</b>
+            </p>
           ) : (
-            <EuiFlexItem>
+            <div>
               <form onSubmit={(event) => submitHandler(event)}>
                 <p>
                   <input
@@ -153,9 +130,9 @@ const FactrakPolicy = ({ currUser, navigateTo, wso }) => {
               </form>
               <br />
               <br />
-            </EuiFlexItem>
+            </div>
           )}
-        </EuiFlexGroup>
+        </EuiText>
       </section>
     </div>
   );
