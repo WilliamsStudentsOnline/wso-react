ff82e774dcb494c175166ff77c508e5c47cebb48
Garett Tok <garettok@hotmail.com>
2020-07-12 18:31:56 +0800

    Updated Footer

--- src/components/Layout/Footer/index.jsx
+++ src/components/Layout/Footer/index.jsx
@@ -1,17 +1,41 @@
 import React from "react";
 import styles from "./Footer.module.scss";
 
-import { EuiSpacer } from "@elastic/eui";
+import { FaInstagram } from "react-icons/fa";
+import { FiMail } from "react-icons/fi";
+import { EuiHorizontalRule, EuiToolTip } from "@elastic/eui";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <small>
-        &copy;
-        {new Date().getFullYear()} Williams Students Online
-      </small>
-      <EuiSpacer size="s" />
-      <small>Contact Us at wso-dev [at] wso.williams.edu</small>
+      <div>
+        <div className={styles.socialLinks}>
+          <EuiToolTip content="Check out our Instagram">
+            <a
+              href="https://www.instagram.com/wsogram/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaInstagram />
+            </a>
+          </EuiToolTip>
+          &emsp;
+          <EuiToolTip content="Email us at wso-dev@williams.edu">
+            <a
+              href="mailto:wso-dev@williams.edu"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FiMail />
+            </a>
+          </EuiToolTip>
+        </div>
+        <EuiHorizontalRule />
+        <small>
+          Copyright &copy; {new Date().getFullYear()} Williams Students Online.
+          All rights reserved.
+        </small>
+      </div>
     </footer>
   );
 };
