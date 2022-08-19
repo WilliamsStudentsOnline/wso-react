f73a4a504d3834b2ffb98de4731b4a3ac994e6a9
Ye Shu <hello@shuye.dev>
2022-08-19 10:15:44 -0400

    fix: Tiny fixes

---
+++ src/setupProxy.js
@@ -0,0 +1,29 @@
+// eslint-disable-next-line import/no-extraneous-dependencies -- dependency of react-scripts
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware("/api", {
+      target: "https://wso-dev.williams.edu",
+      changeOrigin: true,
+      secure: false,
+    })
+  );
+
+  app.use(
+    // note that we do not currently deploy image service on dev server
+    createProxyMiddleware("/pic", {
+      target: "https://wso-dev.williams.edu",
+      changeOrigin: true,
+      secure: false,
+    })
+  );
+
+  app.use(
+    createProxyMiddleware("/courses.json", {
+      target: "https://wso.williams.edu",
+      changeOrigin: true,
+      secure: false,
+    })
+  );
+};
