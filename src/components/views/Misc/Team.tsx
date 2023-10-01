97b31eae332defbfddcd8e91f0821ef75a38800a
Himal Pandey <kailashhimal2000@gmail.com>
2023-10-01 14:23:38 -0400

    Added Tim and Himal in about/team (#264)

--- src/components/views/Misc/Team.tsx
+++ src/components/views/Misc/Team.tsx
@@ -11,6 +11,8 @@ const BoardMap = {
 const ContributorMap = {
   "Max Enis": 12516,
   "Max Kan": 12406,
+  "Himal Pandey": 12851,
+  "Tim Kim": 14087,
   "Henrique Rodrigues": 13670,
   "Christopher Brown": 12444,
   "Milo Chang": 12365,
