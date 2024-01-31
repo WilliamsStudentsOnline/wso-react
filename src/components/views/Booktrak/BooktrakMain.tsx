723ec1c75262493f1399b1a6d4b6fa56fdbe8442
Andrew Megalaa <andrewmuhareb@gmail.com>
2024-01-31 15:31:21 -0500

    added booktrak unavailable page

--- src/components/views/Booktrak/BooktrakMain.tsx
+++ src/components/views/Booktrak/BooktrakMain.tsx
@@ -1,8 +1,12 @@
 // React Imports
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // Component Imports
 import Error404 from "../Errors/Error404";
+
+// Redux/Routing imports
+import { useAppSelector } from "../../../lib/store";
+import { getWSO } from "../../../lib/authSlice";
 
 // External Imports
 import { Routes, Route } from "react-router-dom";
@@ -11,9 +15,34 @@ import BooktrakSearch from "./BooktrakSearch";
 import BooktrakBook from "./BooktrakBook";
 import BooktrakListingForm from "./BooktrakListingForm";
 import BooktrakListings from "./BooktrakListings";
+import "../../stylesheets/Booktrak.css";
 
 const BooktrakMain = () => {
-  return (
+  const wso = useAppSelector(getWSO);
+
+  const [apiAvailable, updateApiAvailable] = useState<boolean | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    let isMounted = true;
+    const checkApi = async () => {
+      try {
+        const response = await wso.booktrakService.healthCheck();
+        if (isMounted) {
+          updateApiAvailable(response.data?.ok ?? false);
+        }
+      } catch {
+        updateApiAvailable(false);
+      }
+    };
+    checkApi();
+    return () => {
+      isMounted = false;
+    };
+  }, [wso]);
+
+  return apiAvailable ? (
     <BooktrakLayout>
       <Routes>
         <Route index element={<BooktrakSearch />} />
@@ -33,6 +62,10 @@ const BooktrakMain = () => {
         <Route path="*" element={<Error404 />} />
       </Routes>
     </BooktrakLayout>
+  ) : (
+    <section className="booktrak-unavailable-container">
+      <h1>BookTrak is currently unavailable.</h1>
+    </section>
   );
 };
 
